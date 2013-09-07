cfd7fb8b3c692023b90f4916706d550f8381cc63
mikillin <sergey.rogachevsky@gmail.com>
2013-09-07 17:23:04 +0300

    Changed IPlayerAI interface

--- src/main/java/org/hexlet/gamexo/ai/IPlayerAI.java
+++ src/main/java/org/hexlet/gamexo/ai/IPlayerAI.java
@@ -6,5 +6,5 @@ package org.hexlet.gamexo.ai;
  * Time: 16:56
  */
 public interface IPlayerAI{
-    public Move doMove() throws AIException;
+    public Move doMove(char[][] matrix) throws AIException;
 }
